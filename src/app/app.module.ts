8de69aa00a95c754e3749a3fd3a8313c1ec9fb81
Nastassia Klishevich <nastassia.kl@gmail.com>
2020-04-04 17:56:33 +0300

    add forms, requests and validation

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+
+import { AppRoutingModule }     from './app-routing/app-routing.module';
+
+import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { AddCustomerComponent } from './add-customer/add-customer.component';
+import { NgxMaskModule, IConfig } from 'ngx-mask'
+
+export const options: Partial<IConfig> | (() => Partial<IConfig>) ={};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent,
+    CustomerDetailsComponent,
+    AddCustomerComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgxMaskModule.forRoot(options)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/customer.ts
@@ -0,0 +1,33 @@
+export class Customer {
+    // id: number;
+    // name: string;
+    // surname: string;
+    // income: number;
+
+    id: number;
+    name: string;
+    middlename:	string 
+    surname:	 string;
+    passportSeries:	string;
+    passportNumber:	string; 
+    department:	 string;
+    dateOfDepartment: number ;
+    dateOfBirth:	number ;
+    passportId:	string; 
+    placeOfBirth:	string ;
+    currentCity:	string; 
+    currentAddress:	string; 
+    telephoneHouse:	string; 
+    telephone:	string ;
+    email: string;
+    placeOfWork: string;
+    position:string; 
+    familyStatus:	string;
+    citizenship:	string; 
+    disability:	string; 
+    pensioner:	boolean;
+    income: number;
+    military: boolean;
+    
+
+}
